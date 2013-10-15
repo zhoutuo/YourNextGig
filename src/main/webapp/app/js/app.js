a65a7b65c6f26854bc401a8be9ec887a944c692c
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-10-15 15:38:27 -0700

    Add front page with no logic

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,7 +1,17 @@
 'use strict';
-angular.module('yng', [
-  'yng.filters',
-  'yng.services',
-  'yng.directives',
-  'yng.controllers'
+var app = angular.module('yng', [
+	'yng.filters',
+	'yng.services',
+	'yng.directives',
+	'yng.controllers'
 ]);
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	.when('/home', {
+		templateUrl: 'partials/search.html',
+		controller: 'searchCtrl'
+	})
+	.otherwise({
+		redirectTo: '/home'
+	});
+}]);

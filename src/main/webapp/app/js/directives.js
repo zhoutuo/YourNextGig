014510c8dabb3e927adcf570b45da90291d2a3ca
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-11-22 21:42:38 -0800

    Add example review page

--- src/main/webapp/app/js/directives.js
+++ src/main/webapp/app/js/directives.js
@@ -1,23 +1,29 @@
 'use strict';
 
-/* Directives */
-
-
-angular.module('yng.directives', []).
-  directive('timeline', [function() {
+angular.module('yng.directives', [])
+.directive('timeline', [function() {
   	return {
   		restrict: 'E',
   		templateUrl: "partials/timeline.html",
   		scope: true,
   		link: function(scope, element, attrs) {
   			// init timeline
-			createStoryJS({
-				type: 'timeline',
-				width: '100%',
-				height: '600',
-				source: 'concerts_example.json',
-				embed_id: 'timelineContent'
-			});
+  			createStoryJS({
+  				type: 'timeline',
+  				width: '100%',
+  				height: '600',
+  				source: 'concerts_example.json',
+  				embed_id: 'timelineContent'
+  			});
   		}
   	}
- }]);
+ }])
+.directive('tiles', [function() {
+    return {
+        restrict: 'E',
+        templateUrl: "partials/tiles.html",
+        scope: true,
+        link: function(scope, element, attrs) {
+        }
+    }
+}]);

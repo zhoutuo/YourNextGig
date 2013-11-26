566f952cbb61887a001295bbc61c9af14ff61af3
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-11-26 12:53:37 -0800

    Add $on event listening for timeline directive.

--- src/main/webapp/app/js/directives.js
+++ src/main/webapp/app/js/directives.js
@@ -1,20 +1,24 @@
 'use strict';
 
 angular.module('yng.directives', [])
-.directive('timeline', [function() {
-  	return {
-  		restrict: 'E',
-  		templateUrl: "partials/timeline.html",
-  		scope: true,
-  		link: function(scope, element, attrs) {
-  			// init timeline
-  			createStoryJS({
-  				type: 'timeline',
-  				width: '100%',
-  				height: '600',
-  				source: 'concerts_example.json',
-  				embed_id: 'timelineContent'
-  			});
-  		}
-  	}
- }]);
+  .directive('timeline', [
+    function() {
+      return {
+        restrict: 'E',
+        templateUrl: "partials/timeline.html",
+        scope: true,
+        link: function(scope, element, attrs) {
+          scope.$on("showTimeline", function(event, timelinejson) {
+            // init timeline
+            createStoryJS({
+              type: 'timeline',
+              width: '100%',
+              height: '600',
+              source: timelinejson,
+              embed_id: 'timelineContent'
+            });
+          });
+        }
+      }
+    }
+  ]);

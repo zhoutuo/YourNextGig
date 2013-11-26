67eb8e8c3ffc2ec368ce704fbb4fdbdc6de715f2
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-11-26 12:51:01 -0800

    search page button is disable by default

--- src/main/webapp/app/js/controllers.js
+++ src/main/webapp/app/js/controllers.js
@@ -1,29 +1,33 @@
 'use strict';
 
 var controllers = angular.module('yng.controllers', []);
-controllers.controller('searchCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-	// declare variables
-	$scope.showLoader = true;
-	// grab geo info based on IP from city-check
-	$http.get('https://city-check.appspot.com/json')
-					.success(function(response) {
-						console.log(response.region_name);
-						$scope.city = response.city;
-						$scope.region = response.region_name;
-						$scope.location = response.human_city;
-					})
-					.error(function() {
-						alert("cannot get the your current city");
-					})
-					.then(function() {
-						// complete event for jsonp
-						$scope.showLoader = false;
-					});
-	$scope.submitLocation = function() {
-		// go to concert page
-		$location.path("/concerts").search({
-			location: $scope.city + ',' + $scope.region
-		});
+controllers.controller('searchCtrl', ['$scope', '$http', '$location',
+	function($scope, $http, $location) {
+		// declare variables
+		$scope.showLoader = true;
+		$scope.disableButton = true;
+		// grab geo info based on IP from city-check
+		$http.get('https://city-check.appspot.com/json')
+			.success(function(response) {
+				console.log(response.region_name);
+				$scope.city = response.city;
+				$scope.region = response.region_name;
+				$scope.location = response.human_city;
+				$scope.disableButton = false;
+			})
+			.error(function() {
+				alert("cannot get the your current city");
+			})
+			.then(function() {
+				// complete event for jsonp
+				$scope.showLoader = false;
+			});
+		$scope.submitLocation = function() {
+			// go to concert page
+			$location.path("/concerts").search({
+				location: $scope.city + ',' + $scope.region
+			});
+		}
 	}
 }]);
 controllers.controller('concertsCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
